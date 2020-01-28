7063a04523fa792a214a759d6c66068ce454d582
Judith Lao <laojudith2014@gmail.com>
2020-01-28 10:40:50 -0500

    setup

---
+++ app/app.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+
+export default class Mastermind extends Component {
+
+  constructor() {
+    super()
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return(
+      <div>Setting Up</div>
+    )
+
+  }
+
+}
---
+++ app/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Mastermind from './app'
+
+ReactDOM.render(
+  <Mastermind />,
+  document.getElementById('app')
+)
