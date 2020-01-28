2dcf6995e493698e451e2f8e37049805eb53e4f3
Judith Lao <laojudith2014@gmail.com>
2020-01-28 16:25:54 -0500

    changed to one component, pushes the guess object into the array

--- app/app.js
+++ app/app.js
@@ -7,35 +7,67 @@ export default class Mastermind extends Component {
     super(props)
     this.state = {
       guesses: [],
+      guess: 0,
       counter: 0,
       number: 0
     }
-    this.incorporateGuess = this.incorporateGuess.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   async componentDidMount() {
     let {data} = await axios.get("https://www.random.org/integers/?num=4&min=0&max=7&col=7&base=10&format=plain&rnd=new")
+    let number = data.replace(/\s/g,'')
     this.setState({
-      number: data
+      number: +number
     })
   }
 
-  async incorporateGuess(guess) {
-    await this.setState({
-      guesses: [...this.state.guesses, guess]
+  handleChange(event) {
+    this.setState({
+      guess: event.target.value
     })
-    console.log("submitguess2",this.state.guesses)
   }
 
+  handleSubmit(event) {
+    event.preventDefault()
+    let guessObject = {}
+    let comment;
+    if (this.state.number == this.state.guess) {
+      comment = "You got it right!"
+    }
+
+    guessObject[this.state.guess] = comment
+    this.setState({
+      guesses: [...this.state.guesses, guessObject]
+    }, ()=>{console.log("number type", this.state.guesses)})
+
+  }
 
   render() {
     return(
       <div>
         <div>{this.state.number}</div>
-        <div>{this.state.guesses}</div>
-        {/* <div>{this.state.guesses.map(guess => <Guess></Guess>)</div> */}
-      <div>Setting Up</div>
-      <Input incorporateGuess={this.incorporateGuess} ></Input>
+        {/* <div>{this.state.guesses.map()}</div> */}
+
+
+        <form>
+        {this.state.invalid ? <div>You have guessed this guess before...</div>: null}
+
+          <div class="col-25">
+          <label htmlFor="type">Guess:</label>
+          </div>
+          <div class="col-75">
+          <input type ="text" name="guess" onChange={this.handleChange}/>
+          </div>
+
+          <button type="button" onClick={this.handleSubmit}>
+          Submit Guess
+          </button>
+
+        </form>
+
+
       </div>
     )
 
