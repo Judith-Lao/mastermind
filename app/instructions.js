c4cc7b47a8d75191061f76d0b697959c4910edf1
Judith Lao <laojudith2014@gmail.com>
2020-02-11 12:00:52 -0500

    instructions are clear

--- app/instructions.js
+++ app/instructions.js
@@ -21,7 +21,13 @@ export default class Instructions extends Component {
     <div>
       <h1>Mastermind Game</h1>
       <p>
-        Hello! This game is called Mastermind. Please choose a game difficulty. This will control number of guesses.
+        Hello! This game is called Mastermind.
+
+        You will have ten guesses to guess a computer generated number.
+        There will be feedback in two cases: if you have a correct number,
+        or a correct number in the correct location.
+
+        Please choose a game difficulty. This will control how many digits the number contains, ten being the hardest, seven being medium level difficulty, and four being easy.
       </p>
     <button className="custom-btn btn-1" type="button" onClick={() => this.chooseDifficulty("easy")}>
     Easy
